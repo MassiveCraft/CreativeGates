d0fa834ddbc1d41ac4181d87e9d4f649b233a386
Olof Larsson <olof@sylt.nu>
2013-05-31 09:29:01 +0200

    Starting the development of CreativeGates V2.

---
+++ src/com/massivecraft/creativegates/old/GateOpenException.java
@@ -0,0 +1,11 @@
+package com.massivecraft.creativegates.old;
+
+public class GateOpenException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public GateOpenException(String message)
+	{
+		super(message);
+	}
+}
---
+++ src/com/massivecraft/creativegates/old/Permission.java
@@ -0,0 +1,29 @@
+package com.massivecraft.creativegates.old;
+
+import org.bukkit.command.CommandSender;
+
+public enum Permission
+{
+	USE("use"),
+	CREATE("create"),
+	DESTROY("destroy"),
+	;
+	
+	public final String node;
+	
+	Permission(final String node)
+	{
+		this.node = "creativegates."+node;
+    }
+	
+	public boolean has(CommandSender sender, boolean informSenderIfNot)
+	{
+		return CreativeGates.p.perm.has(sender, this.node, informSenderIfNot);
+	}
+	
+	public boolean has(CommandSender sender)
+	{
+		return CreativeGates.p.perm.has(sender, this.node, false);
+	}
+	
+}
